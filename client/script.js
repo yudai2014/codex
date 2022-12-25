161fa6e1c215d2852bdc88ccef5a9eb53b1f924a
yudai <yudai2014@users.noreply.github.com>
2022-12-24 23:34:20 -0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://codex-vk61.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json' 
